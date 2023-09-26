1f06e469a4106d0ead9bac63b51dc91edd1d54ca
Priyanshu Gupta <priyanshu2859@gmail.com>
2023-09-26 18:07:19 +0530

    first

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable no-dupe-keys */
+import React from 'react'
+import { useState,useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import {Videos,ChannelCard} from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+const ChannelDetail = () => {
+  const [channelDetail,setChannelDetail]=useState(null);
+  const [videos,setVideos]=useState([]);
+  const {id}=useParams();
+  useEffect(()=>{
+    fetchFromAPI(`channels?part="snippet&id=${id}`).then((data)=>setChannelDetail(data?.items[0]));
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data)=>setVideos(data?.items));
+  },[id])
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{background: "rgb(12,35,150)",background: "radial-gradient(circle, rgba(12,35,150,1) 0%, rgba(233,0,47,1) 100%)",zIndex:10,height:"300px"}}/>
+        <ChannelCard channelDetail={channelDetail} marginTop="-123px"></ChannelCard>
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{mr:{sm:"100px"}}}/>
+        <Videos videos={videos}/>
+      </Box>
+    </Box>
+  )
+}
+
+export default ChannelDetail
